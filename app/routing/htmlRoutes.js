535033daf8843f74fe638f45de2374d35faf680e
Margaret Rumble <megrumble@gmail.com>
2017-11-25 11:42:22 -0500

    survey template made

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,2 +1,17 @@
 //dependencies
 var path = require('path');
+
+//routing
+
+module.exports = function (app) {
+    //HTTP GET Requests
+    //user is redirected to survey page
+    app.get("/survey", function (req, res) {
+        res.sendFile(path.join(__dirname, "/../public/survey.html"));
+    });
+
+    //default route is home page
+    app.get("*", function (req, res) {
+        res.sendFile(Path.join(__dirname, "/../public/home.html"));
+    })
+}
